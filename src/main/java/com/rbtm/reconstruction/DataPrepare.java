9f624d5220645c521cd40c4e0f648f179cdddbf4
alop0715 <oparin.mipt@yandex.ru>
2017-10-29 13:48:49 +0300

    init

---
+++ src/main/java/com/rbtm/reconstruction/DataPrepare.java
@@ -0,0 +1,11 @@
+package com.rbtm.reconstruction;
+
+
+public class DataPrepare {
+    public static void main(String[] args){
+        Thread mainThread = Thread.currentThread();
+
+        System.out.println(mainThread.getName());
+    }
+
+}
---
+++ test.sh
@@ -0,0 +1,2 @@
+gradle build
+java -jar build/libs/3dviz-1.0-SNAPSHOT.jar
